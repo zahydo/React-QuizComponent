e0ea8791f4d0bdcb9c5e06589e89fe7b95a067fa
Santiago Hyun <santiago.hyun@codescrum.com>
2019-08-30 17:17:19 -0500

    Displaying Quiz Questions and Handling Events

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,10 +15,15 @@ class Quiz extends Component {
       <div>
         {isQuizEnd ? 
           <QuizEnd /> : 
-          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+          <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
         }
       </div>
     ); 
+  }
+  showNextQuestion() {
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1 }
+    })
   }
 }
 
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,11 +10,23 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton key={this.props.quiz_question.id} button_text={this.props.quiz_question.answer_options[0]}/>
+            { 
+              this.props.quiz_question.answer_options.map((answer_option, index) => 
+                {
+                  return <QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)}/>
+                }
+              )
+            }
           </ul>
         </section>
       </main>
     )
+  }
+
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler();
+    }
   }
 }
 
